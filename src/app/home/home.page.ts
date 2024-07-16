82dad51545335aa3822835eca2c77f29e19753bb
WilliamAndreoli <willandreoli33@gmail.com>
2024-07-16 13:43:13 -0300

    App Gasolina ou Alcool

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public resultado: String = "Resultado"
+  
+  precoAlcool = ""
+
+  precoGasolina = ""
+
   constructor() {}
+
+  calcular() {
+
+      //Validar se os campos foram preenchidos
+      if (this.precoAlcool && this.precoGasolina) {
+
+          var pAlcool = parseFloat(this.precoAlcool)
+          var pGasolina = parseFloat(this.precoGasolina)
+
+          /*
+          Faz cálculo (precoAlcool / precoGasolina)
+          se: resultado >= 0.7 melhor utilizar gasolina
+          senao: melhor utilizar álcool
+          */
+
+          var res = pAlcool / pGasolina
+          
+          if (res > 0.7) {
+              this.resultado = "Melhor utilizar Gasolina"
+          } else {
+              this.resultado = "Melhor utilizar Álcool"
+          }
+
+      } else {
+
+        this.resultado = "Preencha corretamente os campos"
+
+      }
+
+  }
 
 }
